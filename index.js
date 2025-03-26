b021fa97f21d49f9055018e1b9b5ba040d544055
zaharah <nalunkumazaharahbbosa@gmail.com>
2025-03-26 09:42:28 +0300

    createt arrys

---
+++ index.js
@@ -0,0 +1,20 @@
+//number 1
+let arr1 =[3,7,34,90,12];
+let arr2 =[true,"green","where",12,56];
+let element1 = arr1[arr1.length-1];
+let element2 = arr2[arr2.length-1];
+console.log(element1);
+console.log(element2);
+
+
+
+//number2
+Mypet =["Cow","Birds","Snake","Dog"];
+let Mypets =["Cow","Bird","Snake","Dog"];
+let statement = Mypets.join("");
+console.log(statement)
+
+//number3
+var arr3 =[-5,9,5,3,2,-3,6,8,4,1];
+let y = arr3.sort();
+console.log(y);
