f2ce7dd0155aa69a24f1a0da6320d47f91e7df4f
zaharah <nalunkumazaharahbbosa@gmail.com>
2025-03-27 17:55:00 +0300

    created arrays

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//number 1
+ //number 1
 let arr1 =[3,7,34,90,12];
 let arr2 =[true,"green","where",12,56];
 let element1 = arr1[arr1.length-1];
@@ -19,29 +19,44 @@ var arr3 =[-5,9,5,3,2,-3,6,8,4,1];
 let y = arr3.sort();
 console.log(y);
 
-//number five
-let arr = ["boy", "man", "girl", "school", "girl", "woman"];
 
-let prtAr = [];
-let stmt= [];
-let counts = {};
+//number six
+let text = "renniw";
+let orderedText = text.split('').reduce((acc, ch) => {
+    let pos = 0;
+    while (pos < acc.length && ch > acc[pos]) {
+        pos++;
+    }
+    acc.splice(pos, 0, ch); 
+    return acc;
+}, []).join(''); 
+console.log(orderedText);
 
-arr.forEach(item => {
-    if (counts[item]) {
-        counts[item]++;
+let fruitSet = ["Apple", "Banana", "Jackfruit", "Mango", "pineaple", "Grape", "orange", "Peach", "avocado", "Berries"];
+fruitSet.splice(4, 0, "Tomato");  
+console.log(fruitSet);
+
+
+//number four
+
+let array = ["boy", "man", "girl", "school", "girl", "woman"];
+
+let uniqueArray = [];
+let duplicatesArray = [];
+let elementCount = {};
+
+array.forEach(item => {
+    if (elementCount[item]) {
+        elementCount[item]++;
+        if (elementCount[item] === 2) {
+            duplicatesArray.push(item);
+        }
     } else {
-        counts[item] = 1;
+        elementCount[item] = 1;
+        uniqueArray.push(item);
     }
 });
 
-for (let item in counts) {
-    if (counts[item] === 1) {
-        uniqueArr.push(item);
-    } else {
-        duplicatesArr.push(item);
-    }
-}
+console.log("Array without duplicates:", uniqueArray);
+console.log("Array with duplicates:", duplicatesArray);
 
-console.log("Array without duplicates:", prtAr);
-console.log("Array with duplicates only:", stmt);
-//number 
